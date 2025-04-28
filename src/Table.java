f9f13d97144818624b7e81bd6db0bab3292515d0
goncalofiuza <goncalomfiuza@gmail.com>
2025-04-28 16:17:08 +0200

    Initialize array with random values

--- src/Table.java
+++ src/Table.java
@@ -2,5 +2,8 @@ public class Table {
     public static void main(String[] args){
         int[] table = new int[10];
 
+        for(int i = 0; i < table.length; i++){
+            table[i] = (int)(Math.random() * 100);
+        }
     }
 }

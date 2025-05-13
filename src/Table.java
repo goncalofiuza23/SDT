a99aa8792da633284ca7a8f128517ea105ee321a
goncalofiuza23 <147874337+goncalofiuza23@users.noreply.github.com>
2025-05-13 12:55:52 +0200

    Improve sorting algorithm

--- src/Table.java
+++ src/Table.java
@@ -9,7 +9,15 @@ public class Table {
         System.out.println("Array before sorting:");
         System.out.println(java.util.Arrays.toString(table));
 
-        java.util.Arrays.sort(table);
+        for(int i = 0; i < table.length - 1; i++) {
+            for(j = 0; j < table.length - i  - 1; j++){
+                if(table[i] > table[j + 1]){
+                    int temp = table[j];
+                    table[j] = table[j + 1]; 
+                    table[j + 1] = temp; 
+                }
+            }
+        }
 
         System.out.println("Array after sorting:");
         System.out.println(java.util.Arrays.toString(table));
